1191ada34f8e05e64ee2bf1e596af3adc2eaf82a
Francesco Piero Paolicelli <piersoft@me.com>
2015-10-31 17:53:23 +0100

    short link photo

--- main.php
+++ main.php
@@ -267,18 +267,46 @@ function location_manager($telegram,$user_id,$chat_id,$location)
 				$alert .="\nMappa: ".$shortLink['id'];
 				}
 */
-				$alert .= "\n\n";
+			//	$alert .= "\n\n";
 				if ($diva12[$i]!=NULL) {
-					$alert .="Foto: ".$diva12[$i]."\n\n";
+
+					$longUrl = $diva12[$i];
+					$apiKey = API;
+
+					$postData = array('longUrl' => $longUrl, 'key' => $apiKey);
+					$jsonData = json_encode($postData);
+
+					$curlObj = curl_init();
+
+					curl_setopt($curlObj, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url?key='.$apiKey);
+					curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
+					curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
+					curl_setopt($curlObj, CURLOPT_HEADER, 0);
+					curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+					curl_setopt($curlObj, CURLOPT_POST, 1);
+					curl_setopt($curlObj, CURLOPT_POSTFIELDS, $jsonData);
+
+					$response = curl_exec($curlObj);
+
+					// Change the response json string to object
+					$json = json_decode($response);
+
+					curl_close($curlObj);
+					$shortLink = get_object_vars($json);
+					$alert .="\nFoto: ".$shortLink['id']."\n\n";
+				//	$alert .="Foto: ".$diva12[$i]."\n\n";
 			//		$content = array('chat_id' => $chat_id, 'text' => $diva12[$i]);
 			//		$telegram->sendMessage($content);
 				}
 
 					$alert.="__________________________________";
 
+
+
 			}
 
 			//	echo $alert;
+
 				$chunks = str_split($alert, self::MAX_LENGTH);
 				foreach($chunks as $chunk) {
 					$forcehide=$telegram->buildForceReply(true);
