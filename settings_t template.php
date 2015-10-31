25cc794837825be0a10f65afb8ac3362c1a30960
Francesco Piero Paolicelli <piersoft@me.com>
2015-10-31 13:12:15 +0100

    refine

--- settings_t template.php
+++ settings_t template.php
@@ -6,9 +6,4 @@ define('API',''); // api google shortner
 define('TELEGRAM_BOT',''); // token Telegram Bot
 define('BOT_WEBHOOK', ''); // url assoluto https per start.php
 
-// Your Openstreetmap Query settings
-define('AROUND', 5000);						//Number of meters to calculate radius to search
-define('MAX', 30);							//max number of points to search
-
-
 ?>
