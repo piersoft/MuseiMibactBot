2b9a561411a9aab6d5740c7c8f17770979c64c2f
Francesco Piero Paolicelli <piersoft@me.com>
2016-03-28 21:26:22 +0200

    fix mappa nel dettaglio

--- main.php
+++ main.php
@@ -399,7 +399,34 @@ if ($count > 50){
 			$alert .="\nFoto/Video: ".$shortLink['id'];
 
 		}
+		if ($diva9[$i]!=NULL){
+						$longUrl = "http://www.openstreetmap.org/?mlat=".$diva9[$i]."&mlon=".$diva10[$i]."#map=19/".$diva9[$i]."/".$diva10[$i];
 
+						$apiKey = API;
+
+						$postData = array('longUrl' => $longUrl, 'key' => $apiKey);
+						$jsonData = json_encode($postData);
+
+						$curlObj = curl_init();
+
+						curl_setopt($curlObj, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url?key='.$apiKey);
+						curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
+						curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
+						curl_setopt($curlObj, CURLOPT_HEADER, 0);
+						curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+						curl_setopt($curlObj, CURLOPT_POST, 1);
+						curl_setopt($curlObj, CURLOPT_POSTFIELDS, $jsonData);
+
+						$response = curl_exec($curlObj);
+
+						// Change the response json string to object
+						$json = json_decode($response);
+
+						curl_close($curlObj);
+						$shortLinkMappa = get_object_vars($json);
+						$alert .="\nMappa: ".$shortLinkMappa['id'];
+
+					}
 
 			$alert.="\n\n__________________";
 
